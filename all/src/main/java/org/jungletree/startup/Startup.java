d07eb52f9b7c6b16d8fd6c75fc4e0dae20db6f87
Ella Fox <tinyfluffs@tutanota.com>
2020-05-21 17:29:30 +0200

    Logging cleanup

--- all/src/main/java/org/jungletree/startup/Startup.java
+++ all/src/main/java/org/jungletree/startup/Startup.java
@@ -1,23 +1,20 @@
 package org.jungletree.startup;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.jungletree.api.JungleTree;
 import org.jungletree.api.exception.StartupException;
 
-public class Main {
-    private static final Logger log = LogManager.getLogger(Main.class);
+@Log4j2
+public class Startup {
     
     public static void main(String[] args) {
         var server = JungleTree.server();
-
         try {
             server.start();
         } catch (StartupException ex) {
             log.error(ex);
             System.exit(1);
         }
-
         log.info("Starting JungleTree {} for Minecraft v{}", "0.0.1-SNAPSHOT", server.getHighestSupportedGameVersion().getName());
     }
 }
